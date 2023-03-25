073560c2eed1fbffd9771578d68c1ad67c2fdc88
Abhijeet <kumar.abhi0803@gmail.com>
2023-03-25 22:11:13 +0530

    Pagination

--- src/main/java/com/project/springboot/controller/EmployeeController.java
+++ src/main/java/com/project/springboot/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.project.springboot.controller;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,9 +33,13 @@ public class EmployeeController {
 	private EmployeeRepository employeeRepository;
 	
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployee(){
+	public List<Employee> getAllEmployee(@RequestParam(name="page", required=false, defaultValue = "0") int pageNo){
+		
+		Pageable p = PageRequest.of(pageNo, 5);
+		
 		List<Employee> emp = new ArrayList<>();
-		emp = (List<Employee>)employeeRepository.findAll();
+		Page<Employee> page = employeeRepository.findAll(p);
+		emp = page.getContent();
 //		System.out.println(emp);
 		return emp;
 	}
--- src/main/java/com/project/springboot/repository/EmployeeRepository.java
+++ src/main/java/com/project/springboot/repository/EmployeeRepository.java
@@ -1,9 +1,16 @@
 package com.project.springboot.repository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import com.project.springboot.model.Employee;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+
 	
 }
